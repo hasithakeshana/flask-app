95e687f5c5f8a62eaf88158bd6b3ea49579ac73d
hasithakeshana <hasithakeshana9900@gmail.com>
2021-05-11 16:53:19 +0530

    changed

--- application.py
+++ application.py
@@ -5,3 +5,6 @@ application = Flask(__name__)
 def hello():         
     return 'Hello World'
 
+if __name__ == "__main__":
+    #app.run(host='127.0.0.1', port=8001, debug=True)
+	application.run(debug=True) # running the app
