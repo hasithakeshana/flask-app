9cbc7bf5787a3a3c51b14a301af76ac98aaadf88
hasithakeshana <hasithakeshana9900@gmail.com>
2021-05-11 13:35:22 +0530

    code changed

--- application.py
+++ application.py
@@ -6,11 +6,31 @@ import pandas as pd
 from fbprophet import Prophet
 
 application = Flask(__name__)
-api = Api(application)
+# api = Api(application)
 
-class vegPrices(Resource):
-    def get(self,name):
+# class vegPrices(Resource):
+#     def get(self,name):
 
+#         # pickle file name
+#         file_name = 'prophet_pickle_model'
+
+#         # load the pickle file 
+#         loaded_model = pickle.load(open(file_name,'rb'))
+
+#         # hardcoded input of the user
+#         future_date = pd.DataFrame({'ds':['2018-04-01']})
+
+#         # forecast using the model
+#         out = loaded_model.predict(future_date)
+
+#         # print the output
+#         print("%.2f" % out.yhat[0])
+
+#         return {"data": round(out.yhat[0],2)}
+
+ 
+application.route('/test')
+def hello():
         # pickle file name
         file_name = 'prophet_pickle_model'
 
@@ -26,15 +46,14 @@ class vegPrices(Resource):
         # print the output
         print("%.2f" % out.yhat[0])
 
-        return {"data": round(out.yhat[0],2)}
+        return "welcome"
 
-    def post(self,name):
-        return {"data": name}
-
+       
 
 
 
-api.add_resource(vegPrices,"/vegPrices/<string:name>")
+
+# api.add_resource(vegPrices,"/vegPrices/<string:name>")
 
 if __name__ == "__main__":
     application.run(debug=True)
